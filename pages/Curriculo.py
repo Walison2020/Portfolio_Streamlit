7c777a703c648f78c7ac5b3779ed99b9d582589f
Walison_F_Antunes <66975484+Walison2020@users.noreply.github.com>
2023-07-18 11:46:11 -0300

    Update Curriculo.py

--- pages/Curriculo.py
+++ pages/Curriculo.py
@@ -9,6 +9,16 @@ arquivo_css = diretorio / "pages"/"styles" /"geral1.css"
 arquivo_pdf = diretorio / "pages" / "assets" / "WalisonCurri2023.pdf"
 arquivo_img = diretorio / "pages" / "assets" / "FOTOwa.jpg"
 
+st.markdown(f"""
+            <style>
+            .stApp {{background-image: url("https://images.unsplash.com/photo-1509537257950-20f875b03669?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80"); 
+                     background-attachment: fixed;
+                     background-size: cover}}
+         </style>
+         """, unsafe_allow_html=True)
+
+
+
 # Configurações Geral das Informações#
 
 tit = "Curriculum | Walison Ferreira Antunes"
